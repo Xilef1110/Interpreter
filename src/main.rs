1f9b976276fca7978ded668a853892e885b49038
Felix Stock <74522795+Xilef1110@users.noreply.github.com>
2026-02-01 20:36:14 -0800

    Add* interactive mode

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
 // use std::os::unix::fs;
 
+use std::process;
+
 fn main() {
-    let filepath = std::env::args().nth(1).expect("no file given");
+    let arglength = std::env::args().len();
+    if (arglength > 2) {
+        print!("To many arguments");
+        process::exit(64);
+    } else if (arglength == 2) {
+        run_file(std::env::args().nth(1).expect("no file given"));
+    } else {
+    }
+}
+
+fn run_file(filepath: String) {
     println!("Command line argument: {:?}", filepath);
     let contents = std::fs::read_to_string(filepath).expect("File should have opened");
     println!("File contents:\n{contents}");
+}
+
+fn run_prompt() {
+    loop {}
 }
