2a20d6580e162b95141c7fa896680fb167b61036
Felix Stock <74522795+Xilef1110@users.noreply.github.com>
2026-04-27 14:19:20 -0700

    Add REPL framework

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 // use std::os::unix::fs;
 
+use std::io::Write;
+use std::io::stdin;
+use std::io::stdout;
 use std::process;
 
 fn main() {
     let arglength = std::env::args().len();
-    if (arglength > 2) {
+    if arglength > 2 {
         print!("To many arguments");
         process::exit(64);
-    } else if (arglength == 2) {
+    } else if arglength == 2 {
         run_file(std::env::args().nth(1).expect("no file given"));
     } else {
+        run_prompt();
     }
 }
 
@@ -20,5 +24,23 @@ fn run_file(filepath: String) {
 }
 
 fn run_prompt() {
-    loop {}
+    loop {
+        stdout().flush().unwrap();
+        match stdin().lines().next() {
+            Some(Ok(input)) => {
+                if input.trim() == "exit" {
+                    break;
+                }
+                if input.trim().is_empty() {
+                    continue;
+                }
+                run(input);
+            }
+            _ => {}
+        }
+    }
+}
+
+fn run(input: String) {
+    println!("{}", input);
 }
