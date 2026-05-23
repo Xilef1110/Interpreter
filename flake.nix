afa5141fa19a6bcd9e7c6566437fc112b8ae2381
Felix <74522795+Xilef1110@users.noreply.github.com>
2026-05-23 09:16:52 -0700

    Update flake to use Naersk

--- flake.nix
+++ flake.nix
@@ -3,15 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    naersk.url = "github:nix-community/naersk";
   };
 
   outputs =
     { self, nixpkgs }:
     let
       pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      naerskLib = pkgs.callPackage.naersk { };
     in
     {
-      packages."x86.64-linux".default = pkgs.callPackage ./default.nix { };
+      packages."x86.64-linux".default = naerskLib.buildPackage {
+        src = ./.;
+        buildInputs = [ ];
+        nativeBuildInputs = [ pkgs.pkg-config ];
+      };
       devShells."x86_64-linux".default = pkgs.mkShell {
         buildInputs = with pkgs; [
           cargo
