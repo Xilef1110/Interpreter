1b780a12183ca99e397e662cdf00dbd0dc85dd8c
Felix <74522795+Xilef1110@users.noreply.github.com>
2026-07-16 17:29:30 -0700

    Add: stubs for Parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,7 @@
-use crate::scanner::token::Token;
+use crate::{
+    expr::Expr,
+    scanner::{TokenType, token::Token},
+};
 
 struct Parser {
     tokens: Vec<Token>,
@@ -8,5 +11,50 @@ struct Parser {
 impl Parser {
     pub fn new_parser(tokens: Vec<Token>) -> Parser {
         Parser { tokens, current: 0 }
+    }
+
+    // Recursive Descent Functions
+    fn expression(&self) -> Expr {
+        Expr::Assign
+    }
+
+    fn comparison(&self) -> Expr {
+        Expr::Assign
+    }
+
+    fn term(&self) -> Expr {
+        Expr::Assign
+    }
+
+    fn factor(&self) -> Expr {
+        Expr::Assign
+    }
+
+    fn unary(&self) -> Expr {
+        Expr::Assign
+    }
+
+    fn primary(&self) -> Expr {
+        Expr::Assign
+    }
+
+    // Descent helpers
+    fn match_types(&self, types: Vec<TokenType>) -> bool {
+        false
+    }
+    fn check(&self, ttype: TokenType) -> bool {
+        false
+    }
+    fn advance(&self) -> Token {
+        Token::new_token(TokenType::DOT, "".to_string(), 0)
+    }
+    fn is_at_end(&self) -> bool {
+        false
+    }
+    fn peek(&self) -> Token {
+        Token::new_token(TokenType::DOT, "".to_string(), 0)
+    }
+    fn previous(&self) -> Token {
+        Token::new_token(TokenType::DOT, "".to_string(), 0)
     }
 }
