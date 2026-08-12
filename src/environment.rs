a677d51a8b653c44fdcae892cf01576559a6cf68
Felix <74522795+Xilef1110@users.noreply.github.com>
2026-08-11 20:32:50 -0700

    Add: Impl Environment

--- src/environment.rs
+++ src/environment.rs
@@ -1,3 +1,24 @@
+use crate::{Token, TokenType};
+use anyhow::{Result, anyhow};
+use std::collections::HashMap;
+
 pub struct Environment {
     map: HashMap<String, TokenType>,
+}
+
+impl Environment {
+    pub fn define(&mut self, name: String, ttype: TokenType) {
+        self.map.insert(name, ttype);
+    }
+
+    pub fn get(&self, name: Token) -> Result<TokenType> {
+        match self.map.get(&name.get_lexeme()) {
+            Some(ttype) => Ok(ttype.clone()),
+            None => Err(anyhow!(
+                "Undefined variable '{}': line {}",
+                name.get_lexeme(),
+                name.get_line()
+            )),
+        }
+    }
 }
